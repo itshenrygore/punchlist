77f63041c37faf9255bc64a6171f459f916c41c8
itshenrygore <hgore@ualberta.ca>
2026-05-13 14:08:04 -0600

    Delete src directory

--- src/components/quote-editor/index.js
+++
@@ -1,4 +0,0 @@
-export { default as QuoteItemsEditor } from './QuoteItemsEditor';
-export { default as LineItemCard } from './LineItemCard';
-export { default as CatalogSheet } from './CatalogSheet';
-import './quote-editor.css';
